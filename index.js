505e6e5231a3a3fa0729cea57039fa472e066483
danainjax <danainjax@gmail.com>
2021-05-18 12:42:58 -0400

    commit tree 7de0ba9e0c264fe2638d3f70a8594d7fd0171868

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+// customerName`: Declare a variable in global scope called `customerName` using
+// the `var` keyword.
+var customerName;
+customerName = "bob";
+var bestCustomer;
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+ const leastFavoriteCustomer = 'Susan';
+
+ function changeLeastFavoriteCustomer() {
+     leastFavoriteCustomer = 'BILL';
+ }
+// - `leastFavoriteCustomer` and `changeLeastFavoriteCustomer()`: Now declare a
+
+//   write a function called `changeLeastFavoriteCustomer()` that attempts to
+//   change that constant - notice what JavaScript does when you try to change the
+//   constant.
